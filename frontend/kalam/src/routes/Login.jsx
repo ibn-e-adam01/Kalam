4a26aa04296d43d6cd2e3fb84d33dc378d0727c4
user.name: "Asrar Firdoos <user.email: "theasrarfirdous@gmail.com>
2026-08-01 17:23:28 +0530

    modify: UI responsivenss

--- frontend/kalam/src/routes/Login.jsx
+++ frontend/kalam/src/routes/Login.jsx
@@ -39,26 +39,26 @@ const Login = () => {
 
   return (
    <>
-    <div className='h-screen w-full bg-zinc-300 gap-3 flex'>
+    <div className='md:h-screen h-330 w-100% md:bg-zinc-300 gap-3 flex'>
       <form action="" onSubmit={sendData}>
         <div className='flex flex-col gap-11 items-center justify-center h-full py-4 bg-zinc-900 w-125 '>
-        <h1 className='text-5xl text-zinc-100 font-bold'>LOGIN</h1>
+        <h1 className='md:text-5xl text-6xl  text-zinc-100 font-bold'>LOGIN</h1>
         <div className='flex flex-col gap-4'>
         
         <input onChange={(e) => {
           setEmail(e.target.value)
-        }} type="email" className='h-10 w-72 border-2 border-zinc-400 text-zinc-100 text-center text-lg outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Email' value={Email}/>
+        }} type="email" className='md:h-10 h-13 md:w-72 w-91 border-2 border-zinc-400 text-zinc-100 text-center md:text-lg text-2xl outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Email' value={Email}/>
         <input onChange={(e) => {
           setPassword(e.target.value)
-        }} type="password" className='h-10 w-72 border-2 border-zinc-400 text-zinc-100 text-center text-lg outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Password' value={Password}/>
+        }} type="password" className='md:h-10 h-13 md:w-72 w-91 border-2 border-zinc-400 text-zinc-100 text-center md:text-lg text-2xl outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Password' value={Password}/>
         </div>
-        <button className='text-zinc-100 h-11 w-25 rounded-4xl bg-[#2E4540] hover:bg-[#0c5848] hover:cursor-pointer  active:bg-[#0c5848] active:cursor-pointer hover:scale-98 active:scale-98'>LOGIN</button>
+        <button className='text-zinc-100 md:h-11 h-14 md:text-xl text-2xl mt-3 md:w-25 w-37 rounded-4xl bg-[#2E4540] hover:bg-[#0c5848] hover:cursor-pointer  active:bg-[#0c5848] active:cursor-pointer hover:scale-98 active:scale-98'>LOGIN</button>
 
-        <Link className='text-zinc-300 hover:text-zinc-100 hover:underline active:text-zinc-100 active:underline' to='/'>Don't Have An Account? Sign Up</Link>
+        <Link className='text-zinc-300 md:text-sm text-xl hover:text-zinc-100 hover:underline active:text-zinc-100 active:underline' to='/'>Don't Have An Account? Sign Up</Link>
         </div>
         </form>
 
-        <div className='text-zinc-100'>
+        <div className='text-zinc-100 hidden md:flex'>
 
         </div>
         <div className='hidden md:flex items-center justify-center w-full'>
--- frontend/kalam/src/routes/SignUp.jsx
+++ frontend/kalam/src/routes/SignUp.jsx
@@ -55,15 +55,15 @@ try{
 
   return (
    <>
-    <div className='h-screen w-full bg-zinc-300 gap-3 flex'>
+    <div className='md:h-screen h-330 w-full md:bg-zinc-300 gap-3 flex'>
       <form action="" onSubmit={sendData}>
         <div className='flex flex-col gap-11 items-center justify-center h-full py-4 bg-zinc-900 w-125 '>
-        <h1 className='text-5xl text-zinc-100 font-bold'>SIGN UP</h1>
+        <h1 className='md:text-5xl text-6xl text-zinc-100 font-bold'>SIGN UP</h1>
         <div className='flex flex-col gap-4 justify-center items-center'>
           <div className='flex items-center justify-center gap-1 flex-col'>
         <input onChange={(e) => {
           setUsername(e.target.value)
-        }} type="text" className='h-10 w-72 border-2 border-zinc-400 text-zinc-100 text-center text-lg outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Username' value={Username}/>
+        }} type="text" className='md:h-10 h-13 md:w-72 w-91 md:text-lg text-2xl border-2 border-zinc-400 text-zinc-100 text-center outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Username' value={Username}/>
         {NameisUsed?.map((nameUsed) => (
         <h1 key={nameUsed._id} className='text-xs text-red-400'>{nameUsed?.msg}</h1>
         ))}
@@ -71,18 +71,18 @@ try{
         <div className='flex gap-1 flex-col justify-center items-center'>
         <input onChange={(e) => {
           setEmail(e.target.value)
-        }} type="email" className='h-10 w-72 border-2 border-zinc-400 text-zinc-100 text-center text-lg outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Email' value={Email}/>
+        }} type="email" className='md:h-10 h-13 md:w-72 w-91 md:text-lg text-2xl border-2 border-zinc-400 text-zinc-100 text-center outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Email' value={Email}/>
         {EmailisUsed?.map((emailUsed) => (
         <h1 key={emailUsed._id} className='text-xs text-red-400'>{emailUsed?.message}</h1>
         ))}
         </div>
         <input onChange={(e) => {
           setPassword(e.target.value)
-        }} type="password" className='h-10 w-72 border-2 border-zinc-400 text-zinc-100 text-center text-lg outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Password' value={Password}/>
+        }} type="password" className='md:h-10 h-13 md:w-72 w-91 md:text-lg text-2xl border-2 border-zinc-400 text-zinc-100 text-center outline-none rounded-md hover:border-zinc-200 hover:bg-zinc-950 active:border-zinc-200 active:bg-zinc-950' placeholder='Password' value={Password}/>
         </div>
-        <button className='text-zinc-100 h-11 w-25 rounded-4xl bg-[#2E4540] hover:bg-[#0c5848] hover:cursor-pointer  active:bg-[#0c5848] active:cursor-pointer hover:scale-98 active:scale-98'>SIGNUP</button>
+        <button className='text-zinc-100 md:h-11 h-14 md:text-xl text-2xl mt-3 md:w-25 w-37 rounded-4xl bg-[#2E4540] hover:bg-[#0c5848] hover:cursor-pointer  active:bg-[#0c5848] active:cursor-pointer hover:scale-98 active:scale-98'>SIGNUP</button>
 
-        <Link className='text-zinc-300 hover:text-zinc-100 hover:underline active:text-zinc-100 active:underline' to='/login'>Already Have An Account? Sign In</Link>
+        <Link className='text-zinc-300 md:text-sm text-xl hover:text-zinc-100 hover:underline active:text-zinc-100 active:underline' to='/login'>Already Have An Account? Sign In</Link>
         </div>
         </form>
 
